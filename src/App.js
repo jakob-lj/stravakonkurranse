a057397e2f9e30ab5b2ab479591fde79e7dfcb96
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2021-01-31 19:31:09 +0100

    lexical ordering

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
           display: 'flex'
         }}>
           <div style={innerStyled}>
-            <iframe allowtransparency frameborder="0" height="160" scrolling="no" src="https://www.strava.com/clubs/840512/latest-rides/8a811c1626b6310e2c4ae31b13df7db0b8976c86?show_rides=false" width="300"></iframe>
+            <iframe title={'Basket'} allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/841063/latest-rides/4e1d7bcb6e82dff8415fdaf3c1731e8a91b9b129?show_rides=false' width='300'></iframe>
           </div>
           <div style={innerStyled}>
-            <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/841063/latest-rides/4e1d7bcb6e82dff8415fdaf3c1731e8a91b9b129?show_rides=false' width='300'></iframe>
+            <iframe title={'Volleyball'} allowtransparency frameborder="0" height="160" scrolling="no" src="https://www.strava.com/clubs/840512/latest-rides/8a811c1626b6310e2c4ae31b13df7db0b8976c86?show_rides=false" width="300"></iframe>
           </div>
         </div>
       </header>

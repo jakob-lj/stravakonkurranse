52d884ebd36840605fc2eec25e8eaac8de900642
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2021-01-31 19:30:08 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  const innerStyled = {
+    margin: ' 1em'
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{
+          display: 'flex'
+        }}>
+          <div style={innerStyled}>
+            <iframe allowtransparency frameborder="0" height="160" scrolling="no" src="https://www.strava.com/clubs/840512/latest-rides/8a811c1626b6310e2c4ae31b13df7db0b8976c86?show_rides=false" width="300"></iframe>
+          </div>
+          <div style={innerStyled}>
+            <iframe allowtransparency frameborder='0' height='160' scrolling='no' src='https://www.strava.com/clubs/841063/latest-rides/4e1d7bcb6e82dff8415fdaf3c1731e8a91b9b129?show_rides=false' width='300'></iframe>
+          </div>
+        </div>
       </header>
-    </div>
+    </div >
   );
 }
 
